c2b61cc38e448fccdac19ce3e45a54b031f43b8f
Taylor Jones <jonest6@oregonstate.edu>
2020-03-14 12:41:02 -0500

    Update to include timestamp, support bytes

--- supervisor_stdout.py
+++ supervisor_stdout.py
@@ -1,28 +1,46 @@
+#!/usr/bin/env python
+
 from __future__ import print_function
 import sys
+import datetime
 
-def write_stdout(s):
-    sys.stdout.write(s)
+
+def write_stdout(message):
+    sys.stdout.write(message)
     sys.stdout.flush()
 
-def write_stderr(s):
-    sys.stderr.write(s)
-    sys.stderr.flush()
+def get_timestamp_to_milliseconds():
+    now = datetime.datetime.now()
+    timestamp = now.strftime('%Y-%m-%d %H:%M:%S.%f')
+    return timestamp[:-3]
 
 def main():
-    while 1:
-        write_stdout('READY\n') # transition from ACKNOWLEDGED to READY
-        line = sys.stdin.readline()  # read header line from stdin
-        headers = dict([ x.split(':') for x in line.split() ])
-        data = sys.stdin.read(int(headers['len'])) # read the event payload
-        write_stdout('RESULT %s\n%s'%(len(data), data)) # transition from READY to ACKNOWLEDGED
+    while True:
+        # transition from ACKNOWLEDGED to READY
+        write_stdout('READY\n')
+
+        # read header line from stdin
+        line = sys.stdin.readline()
+        headers = dict([x.split(':') for x in line.split()])
+
+        # read the event payload
+        data = sys.stdin.read(int(headers['len']))
+
+        # transition from READY to ACKNOWLEDGED
+        write_stdout('RESULT %s\n%s' % (len(data.encode('utf-8')), data))
 
 def event_handler(event, response):
-    line, data = response.split('\n', 1)
-    headers = dict([ x.split(':') for x in line.split() ])
-    lines = data.split('\n')
-    prefix = '%s %s | '%(headers['processname'], headers['channel'])
-    print('\n'.join([ prefix + l for l in lines ]))
+    """
+    Prefixes each line of the response message with a timestamp and then
+    prints the message to stdout.
+    """
+    content, data = response.split(b'\n', 1)
+    headers = dict([x.split(b':') for x in content.split()])
+    lines = data.split(b'\n')
+    timestamp = get_timestamp_to_milliseconds()
+    prefix = b'%s %s %s | ' % (timestamp, headers[b'processname'], headers[b'channel'])
+    print(b'\n'.join([prefix + line for line in lines if line]).decode('utf-8'))
+
 
 if __name__ == '__main__':
     main()
